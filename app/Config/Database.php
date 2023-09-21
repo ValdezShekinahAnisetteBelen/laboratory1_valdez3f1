fd3273236379a89865e199ee5d19b48b3d81b47b
Valdez Shekinah Anisette Belen <shekinahvaldez2003@gmail.com>
2023-09-21 19:43:28 +0800

    database

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -29,7 +29,7 @@ class Database extends Config
         'hostname'     => 'localhost',
         'username'     => 'root',
         'password'     => '',
-        'database'     => '',
+        'database'     => 'lab1',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
