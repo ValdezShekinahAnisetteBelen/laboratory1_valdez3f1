04ade8adccbfbda62f8f01e7bd242e233c4c5376
Valdez Shekinah Anisette Belen <shekinahvaldez2003@gmail.com>
2023-09-21 19:10:39 +0800

    continue

--- app/Controllers/ProductController.php
+++ app/Controllers/ProductController.php
@@ -6,8 +6,16 @@ use App\Controllers\BaseController;
 
 class ProductController extends BaseController
 {
+    public function product($product)
+    {
+        echo $product;
+    }
     public function index()
     {
-        //
+       
+    }
+    public function shekinah()
+    {
+       return view('products');
     }
 }
